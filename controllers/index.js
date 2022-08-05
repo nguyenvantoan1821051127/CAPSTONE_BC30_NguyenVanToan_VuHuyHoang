895c56564ea71dd6adf691defb8e7b03c97d4d1a
nguyenvantoan1821051127 <79552710+nguyenvantoan1821051127@users.noreply.github.com>
2022-08-05 15:01:04 +0700

    update

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,12 @@
 function getProduct(){
-    let pronise=axios({
+    let promise = axios ({
         url:'https://shop.cyberlearn.vn/api/Product',
         method:'GET',
     });
-    //thanh cong
-    pronise.then(function(result){
-        console.log(result.data);
-        //goi ham rendartable sau khi lay du lieu tu api ve
+
+    promise.then(function(result){
+        console.log(result.data.content)
+
         renderProduct(result.data.content,'tblProduct');
     })
 }
@@ -15,23 +15,21 @@ function renderProduct(arrPr,idBody){
     for(let index=0; index<arrPr.length; index++){
         let product=arrPr[index];
         htmlConect+=`
-        <div class="rendertable-item">
-            <div class="rendertable-item-inner"">
-                <div class="rendertable-item-inner-top">
-                    <img src="${product.image}" alt="...">
+        <div class="col-4">
+            <div class="item">
+                <img src="${product.image}" alt="...">
+                <div class="info">
+                    <h3>${product.alias}</h3>
+                    <p>${product.shortDescription}</p>
                 </div>
-                <div class="rendertable-item-inner-mid">
-                    <p>${product.alias}</p>
-                    <p><span>${product.shortDescription}</span></p>
-                </div>
-                <div class="rendertable-item-inner-bottom">
+                <div class="content">
                     <a href="./detail.html?productid=${product.id}" class="btn-buyNow">Buy now</a>
                     <span class="btn-gia">${product.price}$</span>
                 </div>
             </div>
         </div>`
     }
-    document.getElementById(idBody).innerHTML = htmlContent;
+    document.getElementById(idBody).innerHTML = htmlConect;
 }
 
 window.onload =function(){
